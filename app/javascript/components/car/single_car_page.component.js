14f8396ec8fda34e20c7a561eac121e5827b3ed3
tk-ni <talkalepanagos@gmail.com>
2021-07-15 00:13:29 +0300

    React single car page table

--- app/javascript/components/car/single_car_page.component.js
+++ app/javascript/components/car/single_car_page.component.js
@@ -5,17 +5,31 @@ import React, { useState, useCallback, useEffect } from 'react'
 import Spinner from '../spinner.component'
 import ErrorComponent from '../error.component'
 
+// Material UI
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableContainer from '@material-ui/core/TableContainer'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
+
 //Services
-import { getCarById } from './../../services/car.service'
+import { getCarById, getCarDrivers } from './../../services/car.service'
+
+//Styles
+import './single_car_page.css'
 
 const SingleCarPage = (props) => {
     const [car, setCar] = useState(null);
+    const [drivers, setDrivers] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState('');
 
     const getCarData = useCallback(async () => {
         setLoading(true);
         let data = await getCarById(props.match.params.id);
+
         if (data) {
             setCar(data);
         } else {
@@ -24,14 +38,81 @@ const SingleCarPage = (props) => {
         setLoading(false);
     }, [])
 
+    const getDriversData = useCallback(async () => {
+        let data = await getCarDrivers(props.match.params.id);
+        if (data) {
+            setDrivers(data);
+        }
+    }, [])
+
     useEffect(() => {
         getCarData();
+        getDriversData();
         return () => {
-            setCar([]);
+            setCar(null);
+            setDrivers([]);
         }
     }, [getCarData]);
+
     return (<div className="single-car-page">
-        {loading ? <Spinner /> : error ? <ErrorComponent e={error} /> : car?.attributes?.title}
+        {loading ? <Spinner /> : error ? <ErrorComponent e={error} /> :
+            <div className="single-car-container">
+                <p className="title">{car?.attributes?.title}
+                    <span className="car-id">
+                        #{car?.id}
+                    </span>
+                </p>
+
+                <p className="car-kind">
+                    Car Kind: {car?.attributes?.kind}
+                </p>
+                <p className="car-color">
+                    Car Color: {car?.attributes?.color}
+                </p>
+
+                {drivers.length > 0 ?
+                    <div className="drivers">
+                        <p className="title">Drivers</p>
+                        <TableContainer component={Paper}>
+                            <Table aria-label="simple table">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>
+                                            ID
+                                        </TableCell>
+                                        <TableCell>
+                                            Name
+                                        </TableCell>
+                                        <TableCell>
+                                            Email
+                                        </TableCell>
+                                        <TableCell>
+                                            Created At
+                                        </TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {drivers.map(driver =>{
+                                        return <TableRow key={driver.id}>
+                                            <TableCell>
+                                                {driver.id}
+                                            </TableCell>
+                                            <TableCell>
+                                                {driver.name}
+                                            </TableCell>
+                                            <TableCell>
+                                                {driver.email}
+                                            </TableCell>
+                                            <TableCell>
+                                                {driver.created_at}
+                                            </TableCell>
+                                        </TableRow>
+                                    })}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </div> : ''}
+            </div>}
     </div>)
 }
 
--- app/javascript/services/car.service.js
+++ app/javascript/services/car.service.js
@@ -3,34 +3,56 @@ import { URL } from './../packs/environments'
 
 export const getCars = async () => {
     let res = await axios.get(URL.API.CARS.GET.getCars());
-    if(res.status !== 200){
+    if (res.status !== 200) {
         return false;
     }
 
-    if(!res.data?.data){
+    if (!res.data?.data) {
         return false;
     }
-    
+
     let cars = [];
-    for( const [key,value] of Object.entries(res.data.data)){
-        let obj = {...value};
+    for (const [key, value] of Object.entries(res.data.data)) {
+        let obj = { ...value };
         cars.push(obj);
     }
 
     return cars;
 }
 
-export const getCarById = async (car_id) =>{
+export const getCarById = async (car_id) => {
     let res = await axios.get(URL.API.CARS.GET.getCarById(car_id));
-    if(res.status !== 200){
+    if (res.status !== 200) {
         return false;
     }
 
-    if(!res.data?.data){
+    if (!res.data?.data) {
         return false;
     }
 
-    let car = {...res.data.data};
+    let car = { ...res.data.data };
 
     return car;
+}
+
+export const getCarDrivers = async (car_id) => {
+    let res = await axios.get(URL.API.CARS.GET.getCarDrivers(car_id));
+
+    if (res.status !== 200) {
+        return false;
+    }
+
+    if (!res.data) {
+        return false;
+    }
+
+
+    let drivers = [];
+    for (const [key, value] of Object.entries(res.data)) {
+        let obj = { ...value };
+        drivers.push(obj);
+    }
+
+    return drivers;
+
 }
