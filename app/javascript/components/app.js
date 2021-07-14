0cf47d97a1fe9226e24c71ffaa19f3b9c8c82433
tk-ni <talkalepanagos@gmail.com>
2021-07-14 13:50:40 +0300

    react directory structure + yarn add axios

--- app/javascript/components/app.js
+++ app/javascript/components/app.js
@@ -1,11 +1,24 @@
+// Dependencies
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import Home from './home.component';
+
+// Components
+import Home from './home.component'
+import CarListPage from './car/car_list/car_list_page.component'
+import NewCarPage from './car/new_car/new_car_page.component'
+import DriverListPage from './driver/driver_list/driver_list_page.component'
+import NewDriverPage from './driver/new_driver/new_driver_page.component'
+import DriversAndCarsPage from './drivers_and_cars/drivers_and_cars_page.component'
+
 const App = () => {
     return (
         <Switch>
             <Route exact path="/" render={() => <Home />} />
-
+            <Route exact path="/car/list" render={() => <CarListPage />} />
+            <Route exact path="/car/new" render={() => <NewCarPage />} />
+            <Route exact path="/driver/list" render={() => <DriverListPage />} />
+            <Route exact path="/driver/new" render={() => <NewDriverPage />} />
+            <Route exact path="/driversandcars" render={() => <DriversAndCarsPage />} />
             {/* Handle 404's */}
             <Route render={() => <Home />} />
         </Switch>
---
+++ app/javascript/components/car/car_list/car_list_page.component.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+
+const CarListPage = () =>{
+
+    useEffect(()=>{
+        
+    }, []);
+
+    return(<div>Car list page</div>)
+}
+
+export default CarListPage;
