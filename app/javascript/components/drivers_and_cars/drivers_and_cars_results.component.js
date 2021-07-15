af9c7f2562c12f69042bba40857cbbc3b7149d85
tk-ni <talkalepanagos@gmail.com>
2021-07-15 05:58:17 +0300

    remove CL

--- app/javascript/components/drivers_and_cars/drivers_and_cars_results.component.js
+++ app/javascript/components/drivers_and_cars/drivers_and_cars_results.component.js
@@ -3,7 +3,6 @@ import CarResultsDisplay from "./car_results_display.component"
 import DriverResultsDisplay from "./driver_results_display.component"
 
 const DriversAndCarsResults = (props) => {
-    console.log(props.category);
     return (<div className="drivers-and-cars-results">
         {props.category == 'cars' ?
             <CarResultsDisplay car={props.results.car} drivers={props.results.drivers} />
